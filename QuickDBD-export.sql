0d6298a59b9a364031a1e767a28602bc5665e76e
warshophysical <73259613+warshophysical@users.noreply.github.com>
2021-01-13 23:29:06 -0600

    Add files via upload

---
+++ QuickDBD-export.sql
@@ -0,0 +1,80 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "employees" (
+    "emp_no" int   NULL,
+    "emp_title_id" int   NOT NULL,
+    "birth_date" date   NULL,
+    "first_name" varchar(30)   NULL,
+    "last_name" varchar(30)   NULL,
+    "sex" varchar(10)   NOT NULL,
+    "hire_date" date   NOT NULL,
+    CONSTRAINT "pk_employees" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "titles" (
+    "title_id" int   NOT NULL,
+    "title" varchar(30)   NOT NULL,
+    CONSTRAINT "pk_titles" PRIMARY KEY (
+        "title_id"
+     )
+);
+
+CREATE TABLE "salaries" (
+    "emp_no" int   NOT NULL,
+    "salary" int   NOT NULL
+);
+
+CREATE TABLE "departments" (
+    "dept_id" int   NOT NULL,
+    "dept_name" varchar(50)   NOT NULL,
+    CONSTRAINT "pk_departments" PRIMARY KEY (
+        "dept_id"
+     )
+);
+
+CREATE TABLE "dept_manager" (
+    "dept_id" int   NOT NULL,
+    "emp_no" int   NOT NULL,
+
+    CONSTRAINT "uc_dept_manager_dept_id" UNIQUE (
+        "dept_id"
+    ),
+    CONSTRAINT "uc_dept_manager_emp_no" UNIQUE (
+        "emp_no"
+    )
+);
+
+CREATE TABLE "dept_emp" (
+    "emp_no" int   NOT NULL,
+    "dept_id" int   NOT NULL,
+
+    CONSTRAINT "uc_dept_emp_emp_no" UNIQUE (
+        "emp_no"
+    ),
+    CONSTRAINT "uc_dept_emp_dept_id" UNIQUE (
+        "dept_id"
+    )
+);
+
+ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title_id" FOREIGN KEY("emp_title_id")
+REFERENCES "titles" ("title_id");
+
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_id" FOREIGN KEY("dept_id")
+REFERENCES "departments" ("dept_id");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_id" FOREIGN KEY("dept_id")
+REFERENCES "departments" ("dept_id");
+
